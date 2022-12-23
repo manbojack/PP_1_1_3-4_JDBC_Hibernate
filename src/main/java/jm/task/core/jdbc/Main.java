d45a86e24f675ee288062acfa1c0e3d7d955b2c9
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-23 20:34:29 +0300

    fixed: используй полиморфизм - обращайся к классу через интерфейс

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,10 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.service.UserService;
 
 public class Main {
-    private final static UserServiceImpl userService = new UserServiceImpl();
+    private final static UserService userService = new UserServiceImpl();
 
     public static void main(String[] args) {
         userService.createUsersTable();
