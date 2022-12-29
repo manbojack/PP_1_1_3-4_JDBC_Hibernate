b47e7967f433a4419aee4198f5cb78bc9cf6f759
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-29 08:24:34 +0300

    added annotation Entity for class User

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -3,8 +3,10 @@ package jm.task.core.jdbc.model;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Entity;
 
 @Table
+@Entity
 public class User {
     @Id
     private Long id;
